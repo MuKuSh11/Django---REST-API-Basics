34b97dfdac518c605c2c6ac54a54d30797662b70
MuKuSh11 <munesh.developer@gmail.com>
2024-07-16 02:11:27 +0530

    Register Student model to Admin, create serializers for StudentModel

--- src/StudentAPI/models.py
+++ src/StudentAPI/models.py
@@ -4,3 +4,7 @@ class Student(models.Model):
     name = models.CharField(max_length=20)
     school = models.CharField(max_length=50)
     age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
