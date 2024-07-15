b8abd1ea8bf4c0446e354fcf7c3ff54b8862ce24
MuKuSh11 <munesh.developer@gmail.com>
2024-07-16 02:00:48 +0530

    Setup urls, create Student model and make migrations

---
+++ src/StudentAPI/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=20)
+    school = models.CharField(max_length=50)
+    age = models.IntegerField()
