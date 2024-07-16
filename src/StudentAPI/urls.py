c886497ec0093c29dbdb8579115b1a3295d0494c
MuKuSh11 <munesh.developer@gmail.com>
2024-07-16 09:47:24 +0530

    Add views for other HTTP API calls - GET, POST, PUT, PATCH, DELETE

--- src/StudentAPI/urls.py
+++ src/StudentAPI/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import overview as OverviewView
+from .views import overview
+from .views import add_data
+from .views import modify_data
+from .views import patch_data
+from .views import get_all
+from .views import delete_data
 
 urlpatterns = [
-    path('', OverviewView, name="overview"),
+    path('', overview, name="overview"),
+    path('add/', add_data, name="addData"),
+    path('modify/<int:id>', modify_data, name="modifyData"),
+    path('patch/<int:id>', patch_data, name="patchData"),
+    path('all/', get_all, name="getAll"),
+    path('delete/<int:id>', delete_data, name="deleteData"),
 ]
--- src/StudentAPI/views.py
+++ src/StudentAPI/views.py
@@ -10,5 +10,63 @@ def overview(request):
     about = {
         "AppName": "RESTful API Basics",
         "Status": "Active",
+        "/all": "Get all objects",
+        "/add": "To add new data",
+        "/modify/<id>": "To modify data, you need to pass all the fields",
+        "/patch/<id>": "To modify data, all the fields not required",
+        "/delete/<id>": "To delete data",
     }
     return Response(about)
+
+@api_view(['POST'])
+def add_data(request):
+    received_data = request.data
+    studentData = StudentSerializer(data = received_data)
+    print(type(studentData))
+
+    if studentData.is_valid():
+        studentData.save()
+        return Response(status=status.HTTP_202_ACCEPTED)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['PUT'])
+def modify_data(request, id):
+    student = Student.objects.get(id = id)
+    studentData = StudentSerializer(instance = student, data = request.data)
+
+    if studentData.is_valid():
+        studentData.save()
+        return Response(studentData.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['PATCH'])
+def patch_data(request, id):
+    student = Student.objects.get(id = id)
+    studentData = StudentSerializer(instance = student, data = request.data, partial = True)
+
+    if studentData.is_valid():
+        studentData.save()
+        return Response(studentData.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['GET'])
+def get_all(request):
+    students = Student.objects.all()
+
+    if students:
+        serializedData = StudentSerializer(students, many = True)
+        return Response(serializedData.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['DELETE'])
+def delete_data(request, id):
+    student = Student.objects.get(id = id)
+    if student:
+        student.delete()
+        return Response(status=status.HTTP_202_ACCEPTED)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
